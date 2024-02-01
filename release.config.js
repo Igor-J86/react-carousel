c2139790a90efcb187e2127e12d46c4bb2184e48
Igor Jorganovic <igor.jorganovic@bring.com>
2024-02-02 00:25:15 +0100

    fix: adjust release config

--- release.config.js
+++ release.config.js
@@ -4,12 +4,7 @@ const config = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    [
-      '@semantic-release/npm',
-      {
-        "pkgRoot": "lib/"
-      }
-    ],
+    '@semantic-release/npm',
     ['@semantic-release/git', {
       'assets': ['CHANGELOG.md','package.json','package-lock.json'],
       'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
